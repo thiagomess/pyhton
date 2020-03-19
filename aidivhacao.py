ef4e7e558a05c300527e1f9f20e9640f6c7c46a0
Thiago Gomes <thiagogomes19@hotmail.com>
2020-03-19 20:17:43 -0300

    Usando random

--- aidivhacao.py
+++ aidivhacao.py
@@ -1,10 +1,13 @@
+import random
+
 print("*******************************")
 print("Bem vindo ao game de advinhação")
 print("*******************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101) #gera um numero inteiro de 1 a 100. Iniciando em 1
 total_de_tentativas = 3
 
+print(numero_secreto)
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas)) # usando o format, popula o {} com o valor https://docs.python.org/3/library/string.html#formatexamples
     # print(f"Tentativa {rodada} de {total_de_tentativas}")  #caso adicione o f fora da string é possivel utilizar assim
