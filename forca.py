ef79698b69e376513de77625758dc565eb665952
Thiago Gomes <thiagogomes19@hotmail.com>
2020-03-22 14:56:37 -0300

    curso 2  - a1 e a2 - Trabalhando com string

--- forca.py
+++ forca.py
@@ -4,5 +4,25 @@ def jogar():
     print("Bem vindo ao game de forca")
     print("**************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        print("jogando...")
+
+        chute = input("Digite qual letra? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if chute.lower() == letra.lower():
+                print("Encontrado a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+
 
     print("Fim do jogo")
+
+
+if __name__ == "__main__":
+    jogar()
