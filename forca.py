93e2bd908a502268597d4d87f7cfabbfe0662dc8
Thiago Gomes <thiagogomes19@hotmail.com>
2020-03-26 22:40:40 -0300

    a7 - Refatorando o projeto

--- forca.py
+++ forca.py
@@ -2,14 +2,46 @@ import random
 
 
 def jogar():
+    imprime_mensagem_inicial()
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = incializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+    tentativas = 7
+
+    while not enforcou and not acertou:
+        chute = chute_jogador()
+
+        if chute in palavra_secreta:
+            altera_letra_pelo_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            tentativas -= 1
+            desenha_forca(tentativas)
+
+        acertou = "_" not in letras_acertadas  # caso nao tenha mais "_" seta true
+        enforcou = tentativas == 0
+        print(letras_acertadas)
+
+    if acertou:
+        print("Parabéns, você acertou a palavra!!")
+    else:
+        print("Poxa não foi desta vez, a palavra era {}".format(palavra_secreta))
+
+
+def imprime_mensagem_inicial():
     print("**************************")
     print("Bem vindo ao game de forca")
     print("**************************")
 
+
+def carrega_palavra_secreta(nome_arquivo="palavras.txt"):  # parametro é opcional
     # with open("palavras.txt") as arquivo: #Essa é outra forma de abrir o arquivo, mesmo caso haja exceção,
     # o arquivo é fechado automaticamente
     try:
-        arquivo = open("palavras.txt", "r")
+        arquivo = open(nome_arquivo, "r")
         palavras = []
 
         for linha in arquivo:
@@ -17,42 +49,80 @@ def jogar():
         arquivo.close()
     except:
         palavras = ["maça", "banana", "morango", "laranja", "tangerina", "caqui", "pessego", "melancia"]
-
     random.shuffle(palavras)
-
     palavra_secreta = palavras[0].upper()
-    letras_acertadas = ["_" for letra in
-                        palavra_secreta]  # podemos criar um for dentro na inicializacao(list comprehension )
 
-    enforcou = False
-    acertou = False
-    tentativas = 6
+    return palavra_secreta
 
-    print(letras_acertadas)
-    while not enforcou and not acertou:
-        chute = input("Digite uma  letra? ")
-        chute = chute.strip().upper()
 
-        if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
-        else:
-            tentativas -= 1
-            print("Você errou, restam {} tentativas".format(tentativas))
+def incializa_letras_acertadas(palavra):
+    return ["_" for letra in
+            palavra]  # podemos criar um for dentro na inicializacao(list comprehension )
 
-        acertou = "_" not in letras_acertadas  # caso nao tenha mais "_" seta true
-        enforcou = tentativas == 0
-        print(letras_acertadas)
 
-    if acertou:
-        print("Parabens, você acertou a palavra!!")
-    else:
-        print("Não foi dessa vez, tente novamente")
+def chute_jogador():
+    chute = input("Digite uma  letra? ")
+    chute = chute.strip().upper()
+    return chute
 
-    print("Fim do jogo")
+
+def altera_letra_pelo_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if erros == 6:
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 5:
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 4:
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 3:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if erros == 2:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if erros == 1:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if erros == 0:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if __name__ == "__main__":
